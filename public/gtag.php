e8ee5613c405707294daac306b22f739dc04d42d
GChiara <girardelli.chiara@gmail.com>
2021-11-10 17:13:39 +0100

    updated data and added gtag

--- public/gtag.php
+++ public/gtag.php
@@ -1,10 +1,9 @@
-<!-- Global Site Tag (gtag.js) - Google Analytics -->
-<!-- Replace CODE-HERE with ID -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=CODE-HERE"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-C4VM0ZHL0W"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'CODE-HERE', { 'anonymize_ip': true });
+  gtag('config', 'G-C4VM0ZHL0W');
 </script>
